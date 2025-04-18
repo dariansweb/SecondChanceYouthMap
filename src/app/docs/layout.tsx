4808c0da43be3865cc7eae18995b906a918f4200
dariansweb <dariansweb@gmail.com>
2025-04-18 13:04:46 -0500

    fix margins of responsived doc pages to get rid of the big black margin gutter

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -6,14 +6,18 @@ export default function DocsLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col md:flex-row min-h-screen">
-      <aside className="md:w-64 border-r border-slate-200 bg-white">
-        <DocsSidebar />
-      </aside>
+    <div className="min-h-screen bg-slate-50 text-slate-700">
+      <div className="flex flex-col md:flex-row w-full">
+        {/* Sidebar */}
+        <aside className="md:w-64 border-r border-slate-200 bg-white">
+          <DocsSidebar />
+        </aside>
 
-      <main className="flex-1 p-6 md:p-10 overflow-y-auto">
-        {children}
-      </main>
+        {/* Main Content */}
+        <main className="flex-1 w-full px-4 sm:px-6 lg:px-8 py-10">
+          {children}
+        </main>
+      </div>
     </div>
   );
 }
